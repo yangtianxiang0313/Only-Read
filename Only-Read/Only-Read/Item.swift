3494daf0c30bc38234d1a26ec3c04933fd13904d
txy <yangtianxiang0313@gmail.com>
2024-09-01 22:02:24 +0800

    Project Init

---
+++ Only-Read/Only-Read/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Only-Read
+//
+//  Created by 杨天翔 on 1/9/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Only-Read/Only-Read/Only_ReadApp.swift
@@ -0,0 +1,32 @@
+//
+//  Only_ReadApp.swift
+//  Only-Read
+//
+//  Created by 杨天翔 on 1/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Only_ReadApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
