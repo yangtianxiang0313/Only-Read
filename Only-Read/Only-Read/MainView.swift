56b3fabb9107a33a7d42f2d6e23c4addf0889dab
txy <yangtianxiang0313@gmail.com>
2024-09-07 21:19:05 +0800

    feat:  remove default code

---
+++ Only-Read/Only-Read/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Only-Read
+//
+//  Created by 杨天翔 on 7/9/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
